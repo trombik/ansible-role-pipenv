214c805f03b583063ef7b8955fb49c1ad17ea625
Tomoyuki Sakurai <y@trombik.org>
2021-07-24 12:34:56 +0700

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,14 +1,15 @@
 require "spec_helper"
 require "serverspec"
 
-package = "pipenv"
-service = "pipenv"
-config  = "/etc/pipenv/pipenv.conf"
-user    = "pipenv"
-group   = "pipenv"
-ports   = [PORTS]
-log_dir = "/var/log/pipenv"
-db_dir  = "/var/lib/pipenv"
+package = "devel/py-pipenv"
+extra_packages = %w[ databases/py-sqlite3 ]
+service = "youtube_dl"
+user    = "youtube-dl"
+group   = "youtube-dl"
+ports   = [8080]
+default_user = "root"
+default_group = "wheel"
+app_dir = "/usr/local/youtube-dl-server"
 
 case os[:family]
 when "freebsd"
@@ -16,34 +17,47 @@ when "freebsd"
   db_dir = "/var/db/pipenv"
 end
 
+describe group group do
+  it { should exist }
+end
+
+describe user user do
+  it { should exist }
+  it { should belong_to_primary_group group }
+end
+
 describe package(package) do
   it { should be_installed }
 end
 
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape("pipenv") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/pipenv") do
-    it { should be_file }
+extra_packages.each do |p|
+  describe package(p) do
+    it { should be_installed }
   end
+end
+
+describe file "/usr/local/etc/rc.d/youtube_dl" do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 755 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
+  its(:content) { should match(/name=youtube_dl/) }
+end
+
+describe file app_dir do
+  it { should exist }
+  it { should be_mode 755 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_directory }
+end
+
+describe command "cd #{app_dir} && pipenv --venv" do
+  let(:sudo_options) { "-u #{user}" }
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should_not match(/No virtualenv/) }
+  its(:stdout) { should match(/virtualenvs\/youtube-dl-server/) }
 end
 
 describe service(service) do
